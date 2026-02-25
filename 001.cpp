e0a28cea9a7ab19de3c79273ef103fa8c8a5d6a8
rajattiwari271 <rajattiwari7611@gmail.com>
2026-02-25 22:06:47 +0530

    calculator & unit convertor

---
+++ 001.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a; 
+    cout<<"Enter the value of a : "<<endl;
+    cin>>a;
+    int b;
+
+    cout<<"Enter the value of b : " <<endl;
+    cin>>b;
+
+    char op;
+    cout<<"Enter the operator : " <<endl;
+    cin>>op;
+
+    switch(op){
+        case '+' : cout<<a+b<<endl;
+        break;
+        case '-' : cout<<a-b<<endl;
+        break;
+        case '/' : cout <<a/b<<endl;
+        break ;
+        case '%' : cout<<a%b<<endl;
+        break;
+
+        default : cout<< "Enter valid operator."<<endl;
+    }
+
+}
