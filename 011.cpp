9b92cb2c8f50d061365d037970a0770979ba1e5a
rajattiwari271 <rajattiwari7611@gmail.com>
2026-04-19 18:27:25 +0530

    pelindrome problem

---
+++ 011.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+bool checkPelindrome(char word []){
+    int i =0;
+    int n = strlen(word);
+    int  j = n -1;
+    while(i <= j){
+        if(word[i]  != word[j]){
+            return false ;
+        }
+
+        else {
+            i ++ ;
+             j--;
+
+        }
+    }
+    return true;
+}
+
+int main (){
+    char arr[100] = "racecar";
+    cout<<"Check Pelindrome : "<<checkPelindrome(arr);
+}
